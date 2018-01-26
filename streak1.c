75902a019ac5ea22cefb70a5fbdf8236652ef2a6
ranjithstreak <35728127+ranjithstreak@users.noreply.github.com>
2018-01-26 09:55:59 +0530

    Create streak1.c

---
+++ streak1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the Year a");
+    scanf("%d",&a);
+    if(a%4==0)
+    printf("It is a leap year");
+    else
+    printf("It is not a leap year");
+}
